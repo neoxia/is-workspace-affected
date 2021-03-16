b9ec6f24a097da6e25867e1afeff69aa459bee3d
Julien Capellari <julien.capellari@neoxia.com>
2021-03-16 15:20:27 +0100

    update: bundle action

--- node_modules/@yarnpkg/pnp/lib/loader/hydrateRuntimeState.d.ts
+++
@@ -1,5 +0,0 @@
-import { RuntimeState, SerializedState } from '../types';
-export declare type HydrateRuntimeStateOptions = {
-    basePath: string;
-};
-export declare function hydrateRuntimeState(data: SerializedState, { basePath }: HydrateRuntimeStateOptions): RuntimeState;
