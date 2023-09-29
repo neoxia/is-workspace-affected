f3208f90e2694e6b75f4e439533f323cedb11535
Julien Capellari <julien.capellari@neoxia.com>
2023-09-29 18:25:38 +0200

    feat: copy jill's project & workspace

---
+++ src/project/project.ts
@@ -0,0 +1,146 @@
+import * as core from '@actions/core';
+import * as glob from '@actions/glob';
+import { Lock } from '@jujulego/utils';
+import normalize, { Package } from 'normalize-package-data';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+import { Workspace } from './workspace';
+
+// Class
+export class Project {
+  // Attributes
+  readonly root: string;
+  private readonly _lock = new Lock();
+
+  private _isFullyLoaded = false;
+  private _mainWorkspace?: Workspace;
+  private readonly _names = new Map<string, Workspace>();
+  private readonly _workspaces = new Map<string, Workspace>();
+
+  // Constructor
+  constructor(root: string) {
+    this.root = path.resolve(root);
+  }
+
+  // Methods
+  private async _loadManifest(dir: string): Promise<Package> {
+    const file = path.resolve(this.root, dir, 'package.json');
+    core.debug(`Loading ${path.relative(this.root, file)} ...`);
+
+    const data = await fs.readFile(file, 'utf-8');
+    const mnf = JSON.parse(data);
+    normalize(mnf, (msg) => core.debug(msg));
+
+    return mnf;
+  }
+
+  private async _loadWorkspace(dir: string): Promise<Workspace> {
+    return await this._lock.with(async () => {
+      let wks = this._workspaces.get(dir);
+
+      if (!wks) {
+        const manifest = await this._loadManifest(dir);
+        wks = new Workspace(dir, manifest, this);
+
+        this._workspaces.set(dir, wks);
+        this._names.set(wks.name, wks);
+      }
+
+      return wks;
+    });
+  }
+
+  async mainWorkspace(): Promise<Workspace> {
+    if (!this._mainWorkspace) {
+      const manifest = await this._loadManifest('.');
+      this._mainWorkspace = new Workspace('.', manifest, this);
+
+      this._names.set(this._mainWorkspace.name, this._mainWorkspace);
+    }
+
+    return this._mainWorkspace;
+  }
+
+  async currentWorkspace(cwd = process.cwd()): Promise<Workspace | null> {
+    let workspace: Workspace | null = null;
+    cwd = path.resolve(cwd);
+
+    for await (const wks of this.workspaces()) {
+      if (cwd.startsWith(wks.cwd)) {
+        workspace = wks;
+
+        if (wks.cwd !== this.root) return wks;
+      }
+    }
+
+    return workspace;
+  }
+
+  async* workspaces(): AsyncGenerator<Workspace, void> {
+    const main = await this.mainWorkspace();
+    yield main;
+
+    if (this._isFullyLoaded) {
+      for (const wks of this._names.values()) {
+        if (wks.name !== main.name) yield wks;
+      }
+    } else {
+      // Load child workspaces
+      const { workspaces = [] } = main.manifest;
+
+      for (const pattern of workspaces) {
+        const globber = await glob.create(path.join(this.root, pattern), { matchDirectories: true })
+
+        for await (const dir of globber.globGenerator()) {
+          try {
+            // Check if dir is a directory exists
+            const file = path.resolve(this.root, dir);
+            const stat = await fs.stat(file);
+
+            if (stat.isDirectory()) {
+              yield await this._loadWorkspace(dir);
+            }
+
+          } catch (error) {
+            if (error.code === 'ENOENT') {
+              continue;
+            }
+
+            throw error;
+          }
+        }
+      }
+
+      this._isFullyLoaded = true;
+    }
+  }
+
+  async workspace(name?: string): Promise<Workspace | null> {
+    // With current directory
+    if (!name) {
+      const dir = path.relative(this.root, process.cwd());
+      return this._loadWorkspace(dir);
+    }
+
+    // Try name index
+    const wks = this._names.get(name);
+
+    if (wks) {
+      return wks;
+    }
+
+    // Load workspaces
+    if (!this._isFullyLoaded) {
+      for await (const ws of this.workspaces()) {
+        if (ws.name === name) {
+          return ws;
+        }
+      }
+
+      this._isFullyLoaded = true;
+    }
+
+    return null;
+  }
+}
---
+++ src/project/workspace.ts
@@ -0,0 +1,119 @@
+import * as core from '@actions/core';
+import { minimatch } from 'minimatch';
+import path from 'node:path';
+import { Package } from 'normalize-package-data';
+import { satisfies } from 'semver';
+
+import { git } from '@/src/git';
+import { Project } from './project';
+
+// Class
+export class Workspace {
+  // Attributes
+  readonly cwd: string;
+
+  private readonly _affectedCache = new Map<string, Promise<boolean>>();
+
+  // Constructor
+  constructor(
+    cwd: string,
+    readonly manifest: Package,
+    readonly project: Project
+  ) {
+    this.cwd = path.resolve(project.root, cwd);
+  }
+
+  // Methods
+  private _satisfies(from: Workspace, range: string): boolean {
+    if (range.startsWith('file:')) {
+      return path.resolve(from.cwd, range.substring(5)) === this.cwd;
+    }
+
+    if (range.startsWith('workspace:')) {
+      range = range.substring(10);
+    }
+
+    return !this.version || satisfies(this.version, range);
+  }
+
+  private async _isAffected(reference: string, pattern = '**'): Promise<boolean> {
+    let diff = await git.diff('--name-only', reference, '--', this.cwd);
+
+    if (pattern !== '**') {
+      const rel = path.relative(git.root, this.cwd);
+      diff = diff.filter(minimatch.filter(path.join(rel, pattern)));
+    }
+
+    if (diff.length > 0) {
+      return true;
+    }
+
+    // Test dependencies
+    const proms: Promise<boolean>[] = [];
+
+    for await (const dep of this.dependencies()) {
+      proms.push(dep.isAffected(reference, pattern));
+    }
+
+    for await (const dep of this.devDependencies()) {
+      proms.push(dep.isAffected(reference, pattern));
+    }
+
+    const results = await Promise.all(proms);
+    return results.some(r => r);
+  }
+
+  async isAffected(reference: string, pattern = '**'): Promise<boolean> {
+    let isAffected = this._affectedCache.get(`${reference},${pattern}`);
+
+    if (!isAffected) {
+      isAffected = this._isAffected(reference, pattern);
+      this._affectedCache.set(reference, isAffected);
+    }
+
+    return await isAffected;
+  }
+
+  private async* _loadDependencies(dependencies: Record<string, string>, kind: string): AsyncGenerator<Workspace, void> {
+    for (const [dep, range] of Object.entries(dependencies)) {
+      const ws = await this.project.workspace(dep);
+
+      if (ws) {
+        if (ws._satisfies(this, range)) {
+          yield ws;
+        } else {
+          core.debug(`Ignoring ${kind} ${ws.reference} as it does not match requirement ${range}`);
+        }
+      }
+    }
+  }
+
+  async* dependencies(): AsyncGenerator<Workspace, void> {
+    if (!this.manifest.dependencies) return;
+
+    for await (const ws of this._loadDependencies(this.manifest.dependencies, 'dependency')) {
+      yield ws;
+    }
+  }
+
+  async* devDependencies(): AsyncGenerator<Workspace, void> {
+    if (!this.manifest.devDependencies) return;
+
+    for await (const ws of this._loadDependencies(this.manifest.devDependencies, 'devDependency')) {
+      yield ws;
+    }
+  }
+
+  // Properties
+  get name(): string {
+    return this.manifest.name;
+  }
+
+  get version(): string {
+    return this.manifest.version;
+  }
+
+  get reference(): string {
+    return this.version ? `${this.name}@${this.version}` : this.name;
+  }
+}
