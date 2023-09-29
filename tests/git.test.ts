e487a36d6cc20d6c2eb8042881b78ddd8ca63e16
Julien Capellari <julien.capellari@neoxia.com>
2023-09-29 11:30:03 +0200

    feat: test git utils

---
+++ tests/git.test.ts
@@ -0,0 +1,72 @@
+import * as core from '@actions/core';
+import { FetchResult, TagResult } from 'simple-git';
+
+import { git } from '@/src/git';
+
+// Setup
+jest.mock('@actions/core');
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+
+  jest.mocked(core.group).mockImplementation((name, fn) => fn());
+});
+
+// Tests
+describe('git.fetch', () => {
+  it('should call git fetch command', async () => {
+    const result = { raw: 'test' } as FetchResult;
+    jest.spyOn(git.git, 'fetch').mockResolvedValue(result);
+
+    // Call
+    await expect(git.fetch('a', 'b')).resolves.toEqual(result);
+
+    expect(git.git.fetch).toHaveBeenCalledWith(['a', 'b']);
+
+    expect(core.group).toHaveBeenCalledWith('git fetch a b', expect.any(Function));
+    expect(core.info).toHaveBeenCalledWith('test');
+  });
+});
+
+describe('git.diff', () => {
+  it('should call git diff command', async () => {
+    jest.spyOn(git.git, 'diff').mockResolvedValue('test\nlife=42');
+
+    // Call
+    await expect(git.diff('a', 'b')).resolves.toEqual([
+      'test',
+      'life=42'
+    ]);
+
+    expect(git.git.diff).toHaveBeenCalledWith(['a', 'b']);
+
+    expect(core.group).toHaveBeenCalledWith('git diff a b', expect.any(Function));
+    expect(core.info).toHaveBeenCalledWith('test\nlife=42');
+  });
+});
+
+describe('git.tags', () => {
+  const result: TagResult = {
+    all: ['a', 'b'],
+    latest: 'b',
+  };
+
+  beforeEach(() => {
+    jest.spyOn(git.git, 'tags').mockResolvedValue(result);
+  });
+
+  it('should run git tag to load tags', async () => {
+    await expect(git.tags()).resolves.toEqual(result);
+
+    expect(git.git.tags).toHaveBeenCalled();
+  });
+
+  it('should run git fetch and git tag to load tags', async () => {
+    jest.spyOn(git.git, 'fetch').mockResolvedValue({} as FetchResult);
+
+    await expect(git.tags({ fetch: true })).resolves.toEqual(result);
+
+    expect(git.git.fetch).toHaveBeenCalledWith(['--tags']);
+    expect(git.git.tags).toHaveBeenCalled();
+  });
+});
